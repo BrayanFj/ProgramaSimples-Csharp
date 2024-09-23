77d0709a50dba7acd79b10494fba337f54436154
Brayan Fernandes <132934859+BrayanFj@users.noreply.github.com>
2024-09-22 23:29:07 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SimpleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá! Bem-vindo ao programa de soma simples.");
+            
+            Console.Write("Por favor, insira seu nome: ");
+            string nome = Console.ReadLine();
+            
+            Console.WriteLine($"Prazer em te conhecer, {nome}!");
+            
+            Console.Write("Digite o primeiro número: ");
+            double numero1 = Convert.ToDouble(Console.ReadLine());
+            
+            Console.Write("Digite o segundo número: ");
+            double numero2 = Convert.ToDouble(Console.ReadLine());
+
+            double soma = numero1 + numero2;
+
+            Console.WriteLine($"A soma de {numero1} e {numero2} é: {soma}");
+
+            Console.WriteLine("Obrigado por usar o programa. Até mais!");
+        }
+    }
+}
